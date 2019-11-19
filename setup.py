19c6846bb3128fa208375ace5043a4b314247de8
jhernandez <jhernandez.sld@gmail.com>
2019-11-18 19:02:03 -0600

    Added setup and configured test

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name="FunctionalTools",
+    version="0.0.1",
+    author="Jose hdz",
+    author_email="jhernandez.sld@gmail.com",
+    description=("Tools for fp"),
+    license="MIT",
+    keywords="python functional programming",
+    url="http://github.com/ppgodel/FunctionalTools",
+    packages=['rx'],
+)
--- test/functor_test.py
+++ test/functor_test.py
@@ -1,48 +1,60 @@
 #!/usr/bin/env python3
 from context import functor
-from typing import Any
+from unittest import TestCase
 
 
-def add3(x: int) -> int:
-    return x + 3
+class FunctorTest(TestCase):
+    def test_identity(self):
+        def identity(x):
+            return x
+        value = 'hola'
+        functor_identity = functor(identity)
+        return_value = functor_identity(value)
+        self.assertEqual(value, return_value)
+
+    def test_composition(self):
+        def fn_f(x):
+            return x ** 3
+
+        def fn_g(x):
+            return x + 10
+
+        def gof(x):
+            return fn_g(fn_f(x))
+
+        value = 7
+        composited = functor(fn_f).map(fn_g)
+        self.assertEqual(composited(value), gof(value))
 
 
-def multiply5(x: int) -> int:
-    return x*5
+# functor_1 = functor(add3)  # type Functor[int]
+# functor_post = functor_1.map(multiply5).map(to_string)
+# print(functor_post(4.0))
+# functor_2 = functor.of(4.0)
+# functor_pre = functor_2.map(add3).map(multiply5).map(to_string)
+# print(functor_pre())
+# print(f'combined1: {functor_post(functor_2())}')
+# functor_3 = functor.of('Hola')  # type Functor[str]
+# functor_pre_str = functor_3.map(len).map(add3).map(multiply5).map(to_string)
+# print(functor_pre_str())
+# # side efect horrible que obtiene un diccionario del mundo real
+# functor_dict = functor.of({'name': 'Jose'})
+#
+# functor_get_name = functor(lambda dic: dic.get('name', 'NoName')).\
+#     map(str.upper).map(lambda x: f'my name is {x}')  # pure function
+# name_val = functor_get_name(functor_dict())
 
 
-def to_string(x: Any) -> str:
-    return f'str: {str(x)}'
-
-
-functor_1 = functor(add3)  # type Functor[int]
-functor_post = functor_1.map(multiply5).map(to_string)
-print(functor_post(4.0))
-functor_2 = functor.of(4.0)
-functor_pre = functor_2.map(add3).map(multiply5).map(to_string)
-print(functor_pre())
-print(f'combined1: {functor_post(functor_2())}')
-functor_3 = functor.of('Hola')  # type Functor[str]
-functor_pre_str = functor_3.map(len).map(add3).map(multiply5).map(to_string)
-print(functor_pre_str())
-# side efect horrible que obtiene un diccionario del mundo real
-functor_dict = functor.of({'name': 'Jose'})
-
-functor_get_name = functor(lambda dic: dic.get('name', 'NoName')).\
-    map(str.upper).map(lambda x: f'my name is {x}')  # pure function
-name_val = functor_get_name(functor_dict())
-
-
-def side_effect_save(x: Any) -> None:
-    print(f'printed: {x}')
-
-
-functor_print = functor(side_effect_save)
-functor_print.combine(functor_get_name.combine(functor.of({})))()
-functor_print.combine(functor_get_name).combine(functor.of({}))()
-# backwards first the final side effect and xform are combined
-# finally the inital side effect is combined and then executed
-functor_print.combine(functor_get_name).combine(functor_dict)()
-# forward, the initial side effect is maped to the xform and then
-# mapped to the finall side effect, then it is executed
-functor_dict.map(functor_get_name).map(functor_print)()
+# def side_effect_save(x: Any) -> None:
+#     print(f'printed: {x}')
+#
+#
+# functor_print = functor(side_effect_save)
+# functor_print.combine(functor_get_name.combine(functor.of({})))()
+# functor_print.combine(functor_get_name).combine(functor.of({}))()
+# # backwards first the final side effect and xform are combined
+# # finally the inital side effect is combined and then executed
+# functor_print.combine(functor_get_name).combine(functor_dict)()
+# # forward, the initial side effect is maped to the xform and then
+# # mapped to the finall side effect, then it is executed
+# functor_dict.map(functor_get_name).map(functor_print)()
